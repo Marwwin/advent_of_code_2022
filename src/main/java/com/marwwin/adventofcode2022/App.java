4da907c5db3ee68077a1b095c36fd18219b424ec
makankko <markus.kankkonen@nokia.com>
2022-12-08 20:21:18 +0200

    fixed app

--- src/main/java/com/marwwin/adventofcode2022/App.java
+++ src/main/java/com/marwwin/adventofcode2022/App.java
@@ -8,41 +8,49 @@ import com.marwwin.adventofcode2022.day3.Day3;
 import com.marwwin.adventofcode2022.day4.Day4;
 import com.marwwin.adventofcode2022.day5.Day5;
 import com.marwwin.adventofcode2022.day6.Day6;
+import com.marwwin.adventofcode2022.day7.Day7;
+import com.marwwin.adventofcode2022.day8.Day8;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        showHeader();
+        public static void main(String[] args) throws IOException {
+                showHeader();
 
-        long startTime = System.nanoTime();
+                long startTime = System.nanoTime();
 
-        // Day1 day1 = new Day1(false);
-        // day1.printResults();
-// 
-        // Day2 day2 = new Day2(false);
-        // day2.printResults();
-// 
-        // Day3 day3 = new Day3(false);
-        // day3.printResults();
-// 
-        // Day4 day4 = new Day4(false);
-        // day4.printResults();
-// 
-        // Day5 day5 = new Day5(false);
-        // day5.printResults();
+                Day1 day1 = new Day1(false);
+                day1.printResults();
 
-        com.marwwin.adventofcode2021.day1.Day1 day1 = new com.marwwin.adventofcode2021.day1.Day1(false);
-        long endTime = System.nanoTime();
-        System.out.println("All solutions took " + (endTime - startTime) / 1000 + " μs");
+                Day2 day2 = new Day2(false);
+                day2.printResults();
 
+                Day3 day3 = new Day3(false);
+                day3.printResults();
 
+                Day4 day4 = new Day4(false);
+                day4.printResults();
 
-    }
+                Day5 day5 = new Day5(false);
+                day5.printResults();
 
-    private static void showHeader() {
-        System.out.println();
-        System.out.println("#############################");
-        System.out.println("#### Advent Of Code 2022 ####");
-        System.out.println("#############################");
-        System.out.println();
-    }
+                Day6 day6 = new Day6(false);
+                day6.printResults();
+
+                Day7 day7 = new Day7(false);
+                day7.printResults();
+
+                Day8 day8 = new Day8(false);
+                day8.printResults();
+
+                long endTime = System.nanoTime();
+                System.out.println("All solutions took " + (endTime - startTime) / 1000 + " μs");
+
+        }
+
+        private static void showHeader() {
+                System.out.println();
+                System.out.println("#############################");
+                System.out.println("#### Advent Of Code 2022 ####");
+                System.out.println("#############################");
+                System.out.println();
+        }
 }
